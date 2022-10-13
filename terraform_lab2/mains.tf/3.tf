8e2ff25b24cd12ec44f9393f266cfe88f4ba9dc5
Ekene Umegakwe <95085212+kingsmandralph@users.noreply.github.com>
2022-10-13 13:57:49 +0100

    Update 3.tf

--- terraform_lab2/mains.tf/3.tf
+++ terraform_lab2/mains.tf/3.tf
@@ -1,10 +1,10 @@
 resource "aws_subnet" "cba_private" {
-  vpc_id     = aws_vpc.my_vpc
+  vpc_id     = aws_vpc.cba_vpc.id
   cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone = "us-west-2a"
+  availability_zone = "eu-west-2a"
 
   tags = {
-    Name = "ApachePublicSubnet"
+    Name = "ApachePrivateSubnet"
   }
 }
